cb87b4b94f96b88e20f881e42cabcaca11ee8749
Marco Rossi <marco.rossi@microsoft.com>
2018-10-19 16:55:49 -0400

    rl_sim: activate/deactivate and outcomes via rest

--- reinforcement_learning/examples/python/rl_sim.py
+++ reinforcement_learning/examples/python/rl_sim.py
@@ -1,6 +1,9 @@
 import argparse
 import random
 import time
+import uuid
+import requests
+import json
 
 import rl_client
 
@@ -40,13 +43,21 @@ class rl_sim:
         self._rl_client = rl_client.live_model(self.config, my_error_callback())
         self._rl_client.init()
 
-        tp1 = {'HerbGarden': 0.3, "MachineLearning": 0.2 }
-        tp2 = {'HerbGarden': 0.1, "MachineLearning": 0.4 }
+        tp1 = {'HerbGarden': 0.3, "MachineLearning": 0.2, "Tennis": 0.5 }
+        tp2 = {'HerbGarden': 0.1, "MachineLearning": 0.4, "Tennis": 0.01 }
 
-        self._actions = ['HerbGarden', 'MachineLearning']
+        self._actions = ['HerbGarden', 'MachineLearning', 'Tennis']
         self._people = [
             person('rnc', 'engineering', 'hiking', 'spock', tp1),
             person('mk', 'psychology', 'kids', '7of9', tp2)]
+            
+        # REST-API settings
+        if self._options.rew_via_rest:
+            appId = json.load(open(self._options.json_config))["ApplicationID"]
+            self.rest_url = 'https://ds.microsoft.com/api/v2/'+appId+'/reward/'
+            self.rest_site = 'site://'+appId
+            self.rest_session = requests.Session()
+            self.rest_session.headers.update({'Content-Type': 'application/json', 'Accept':'application/json'})
 
     def loop(self):
         round = 0
@@ -63,21 +74,32 @@ class rl_sim:
                 shared_features = person.get_features()
                 action_features = '"_multi":[' + ','.join('{"a":{"topic":"'+action+'"}}' for action in self._actions) + ']'
                 context = '{' + shared_features + ',' + action_features + '}'
+                
+                event_id = str(uuid.uuid4())+'-'
 
-                model_id, chosen_action_id, actions_probabilities, event_id = self._rl_client.choose_rank(context)
+                model_id, chosen_action_id, actions_probabilities = self._rl_client.choose_rank(event_id, context, deferred=self._options.do_deferred)
 
+                self._rl_client.report_action_taken(event_id)
+                
                 stats[person._id][chosen_action_id][1] += 1
                 ctr[1] += 1
 
                 outcome = person.get_outcome(self._actions[chosen_action_id])
                 if outcome != 0:
-                    self._rl_client.report_outcome(event_id, outcome)
+                    if self._options.rew_via_rest:
+                        r2 = self.rest_session.post(self.rest_url+event_id, json=outcome)
+                        if r2.status_code != 200 or r2.headers.get('x-msdecision-src', None) != self.rest_site:
+                            err[1] += 1
+                            print('Reward Error - status_code: {}; headers: {}'.format(r2.status_code,r2.headers))
+                    else:
+                        self._rl_client.report_outcome(event_id, outcome)
                     ctr[0] += 1
                     stats[person._id][chosen_action_id][0] += 1
 
-                print('Round: {}, ctr: {:.1%}'.format(round, ctr[0]/ctr[1]), stats)
+                if round % 1000 == 0:
+                    print('Round: {}, ctr: {:.1%}'.format(round, ctr[0]/ctr[1]), stats, model_id, self._options.do_deferred)
 
-                time.sleep(0.1)
+                time.sleep(0.001)
             except Exception as e:
                 print(e)
                 time.sleep(2)
@@ -88,6 +110,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--json_config', help='client json config', required=True)
     parser.add_argument('--num_rounds', help='number of rounds (default: infinity)', type=int, default=-1)
+    parser.add_argument('--rew_via_rest', help='Send rewards via REST-API (default: false)', action='store_true')
+    parser.add_argument('--do_deferred', help='Enable deferred actions (default: false)', action='store_true')
 
     vm = parser.parse_args()
     sim = rl_sim(vm)
