0cae09267eca1698f3d8b574b700320c88d3c316
Marco Rossi <marco.rossi@microsoft.com>
2019-04-02 15:46:50 -0700

    Simulator 2.0

--- cs/examples/simulator/VowpalWabbitSimulator.cs
+++ cs/examples/simulator/VowpalWabbitSimulator.cs
@@ -1,4 +1,3 @@
-﻿//using Microsoft.Research.MultiWorldTesting.ExploreLibrary;
 using Newtonsoft.Json;
 using System;
 using System.IO;
@@ -19,11 +18,11 @@ namespace simulator
 
             public float[] PDF { get; }
 
-            public SimulatorExample(int numActions, int sharedContext)
+            public SimulatorExample(int numActions, int sharedContext, float minP, float maxP)
             {
-                // generate distinct per user context with 2 seperate prefered actions
-                this.PDF = Enumerable.Range(0, numActions).Select(_ => 0.005f).ToArray();
-                this.PDF[sharedContext] = 0.03f;
+                // generate distinct per user context with 1 prefered action
+                this.PDF = Enumerable.Range(0, numActions).Select(_ => minP).ToArray();
+                this.PDF[sharedContext] = maxP;
 
                 this.exampleBuffer = new byte[32 * 1024];
 
@@ -39,13 +38,15 @@ namespace simulator
                             U = new { C = sharedContext.ToString() },
                             _multi = Enumerable
                                 .Range(0, numActions)
-                                .Select(i => new { A = new { Constant = 1, Id = i.ToString() }, B = new { Id = i.ToString() } })
+                                // .Select(i => new Dictionary<int, Dictionary<string, int>> { { 0, new Dictionary<string, int> { { i.ToString(), 1 } } } }) // new { A = i.ToString() })
+                                .Select(i => new { A = new { Constant = 1, Id = i.ToString() }, B = new { Id = i.ToString() }, X = new { Constant = 1, Id = (numActions*sharedContext + i).ToString() } })
                                 .ToArray()
                         },
                         p = Enumerable.Range(0, numActions).Select(i => 0.0f).ToArray()
                     });
 
-                Console.WriteLine(str);
+                //Console.WriteLine(str);
+                //Environment.Exit(0);
 
                 // allow for \0 at the end
                 this.length = Encoding.UTF8.GetBytes(str, 0, str.Length, exampleBuffer, 0);
@@ -79,45 +80,41 @@ namespace simulator
             }
         }
 
-        public static void Run(string ml_args, int initial_random, int tot_iter, int mod_iter, int rewardSeed, ulong vwSeed, int exp_iter, int numContexts, int numActions, string ml_args_snips)
+        public static void Run(string ml_args, int tot_iter, int mod_iter, int rnd_seed, int numContexts, int numActions, float minP, float maxP, float baseCost, int pStrategy)
         {
             // byte buffer outside so one can change the example and keep the memory around
             var exampleBuffer = new byte[32 * 1024];
 
-            var randGen = new Random(rewardSeed);
-            var userGen = new Random();
+            var randGen = new Random(rnd_seed);
 
             var simExamples = Enumerable.Range(0, numContexts)
-                .Select(i => new SimulatorExample(numActions, i))
+                .Select(i => new SimulatorExample(numActions, i, minP, maxP))
                 .ToArray();
 
             var scorerPdf = new float[numActions];
-            var histPred = new int[numActions, numContexts];
-            var histPred2 = new int[numActions, numContexts];
-            var histActions = new int[numActions, numContexts];
-            var histCost = new int[numActions, numContexts];
-            var histContext = new int[numContexts];
+            //var histPred = new int[numActions, numContexts];
+            //var histPred2 = new int[numActions, numContexts];
+            //var histActions = new int[numActions, numContexts];
+            //var histCost = new int[numActions, numContexts];
+            //var histContext = new int[numContexts];
             int clicks = 0;
-            double snips_num = 0, snips_den = 0;
+            int goodActions = 0;
+            int goodActionsSinceLast = 0;
 
-            using (var learner = new VowpalWabbit(ml_args))
-            using (var learner2 = new VowpalWabbit(ml_args_snips))
+            using (var learner = new VowpalWabbit(ml_args + " --quiet"))
             {
-                VowpalWabbit scorer = null;
-
-                scorer = new VowpalWabbit("--cb_explore_adf --epsilon 1 --quiet");
                 for (int i = 1; i <= tot_iter; i++)
                 {
                     // sample uniform among users
-                    int userIndex = userGen.Next(simExamples.Length);
+                    int userIndex = randGen.Next(simExamples.Length);
                     var simExample = simExamples[userIndex];
                     var pdf = simExample.PDF;
 
-                    histContext[userIndex]++;
+                    //histContext[userIndex]++;
 
                     using (var ex = simExample.CreateExample(learner))
                     {
-                        var scores = ex.Predict(VowpalWabbitPredictionType.ActionProbabilities, scorer);
+                        var scores = ex.Predict(VowpalWabbitPredictionType.ActionProbabilities, learner);
 
                         var total = 0.0;
 
@@ -140,62 +137,89 @@ namespace simulator
                             }
                         }
 
-                        int modelAction = (int)scores[0].Action;
-                        if (i > initial_random)
-                            histPred[modelAction, userIndex] += 1;
-                        histActions[topAction, userIndex] += 1;
+                        int modelAction = (int)scores[0].Action; // scorerPdf.ToList().IndexOf(scorerPdf.Max());
+                        //histPred[modelAction, userIndex] += 1;
+                        //histActions[topAction, userIndex] += 1;
+                        if (topAction == userIndex)
+                        {
+                            goodActions += 1;
+                            goodActionsSinceLast += 1;
+                        }
+
+                        //Console.Out.WriteLine($"{topAction} {modelAction} {string.Join(",", scorerPdf)}");
 
                         // simulate behavior
-                        float cost = 0;
+                        float cost = baseCost;
                         if (randGen.NextDouble() < pdf[topAction])
                         {
-                            cost = -1;
-                            histCost[topAction, userIndex] += 1;
+                            cost -= 1;
+                            //histCost[topAction, userIndex] += 1;
                             clicks += 1;
+                            //Console.WriteLine($"iter: {i} topAction: {topAction} p:{scorerPdf[topAction]}");
                         }
 
-                        ex.Examples[topAction].Label = new ContextualBanditLabel((uint)topAction, cost, scorerPdf[topAction]);
-
-                        // simulate delay
-                        if (i >= initial_random && (i % exp_iter == 0))
+                        float pReported = scorerPdf[topAction];
+                        switch (pStrategy)
                         {
-                            ExportScoringModel(learner, ref scorer);
+                            case 1:
+                                pReported = 1.0f / numActions;
+                                break;
+                            case 2:
+                                pReported = Math.Max(pReported, 0.5f);
+                                break;
+                            case 6:
+                                pReported = Math.Max(pReported, 0.9f);
+                                break;
+                            case 7:
+                                pReported = 0.9f;
+                                break;
+                            case 13:
+                                pReported = 0.5f;
+                                break;
                         }
+
+                        ex.Examples[topAction].Label = new ContextualBanditLabel((uint)topAction, cost, pReported);
 
                         // invoke learning
                         var oneStepAheadScores = ex.Learn(VowpalWabbitPredictionType.ActionProbabilities, learner);
-                        histPred2[oneStepAheadScores[0].Action, userIndex] += 1;
-
-                        var oneStepAheadScores2 = ex.Learn(VowpalWabbitPredictionType.ActionProbabilities, learner2);
-
-                        // SNIPS
-                        snips_num -= oneStepAheadScores2.First(f => f.Action == topAction).Score * cost / scorerPdf[topAction];
-                        snips_den += oneStepAheadScores2.First(f => f.Action == topAction).Score / scorerPdf[topAction];
+                        //histPred2[oneStepAheadScores[0].Action, userIndex] += 1;
 
                         if (i % mod_iter == 0 || i == tot_iter)
                         {
-                            Console.WriteLine(JsonConvert.SerializeObject(new
-                            {
-                                Iter = i,
-                                clicks,
-                                CTR = clicks / (float)i,
-                                aveLoss = learner.PerformanceStatistics.AverageLoss,
-                                CTR_snips = snips_num / snips_den,
-                                CTR_ips = snips_num / (float)i,
-                                aveLoss2 = learner2.PerformanceStatistics.AverageLoss,
-                                snips_num,
-                                snips_den,
-                                histActions,
-                                histPred,
-                                histCost,
-                                histContext,
-                                pdf
-                            }));
+                            //Console.WriteLine($"Iter {i}");
+                            //Console.WriteLine($"Clicks {clicks}");
+                            //Console.WriteLine($"CTR {clicks / (float)i}");
+                            //Console.WriteLine("Hists:");
+                            //for (int j = 0; j < pdf.Length; j++)
+                            //    Console.WriteLine(j + ": " + histActions[j] + " - " + histPred[j] + "/" + histPred2[j] + " - " + histCost[j] + " - " + pdf[j] + " - " + (histCost[j] / (float)histActions[j]));
+                            //Console.WriteLine();
+
+                            //foreach (var currEx in ex.Examples)
+                            //{
+                            //    var ns = currEx.First();
+                            //    //foreach (var ns in currEx)
+                            //    {
+                            //        //Console.Write($"{ns.Index} ");
+
+                            //        //foreach (var fs in ns)
+                            //        {
+                            //            var fs = ns.First();
+                            //            Console.Write($"{fs.Weight} ");
+                            //            //Console.WriteLine($"{fs.FeatureIndex}:{fs.X}:{fs.WeightIndex}:{fs.Weight}");
+                            //        }
+                            //    }
+                            //}
+                            //Console.Write($"{ex.Examples.First().Last().First().Weight} ");
+                            //Console.WriteLine();
+
+                            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8}", ml_args, numActions, baseCost, pStrategy, rnd_seed, i, clicks/(float)i, goodActions, goodActionsSinceLast);
+
+                            //Console.Out.WriteLine($"{userIndex} {topAction} {modelAction} {string.Join(",", scorerPdf)}");
+
+                            goodActionsSinceLast = 0;
                         }
                     }
                 }
-                Console.WriteLine("---------------------");
-                scorer?.Dispose();
             }
         }
     }
