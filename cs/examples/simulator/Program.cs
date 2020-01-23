7ce76e563cbe39c34026f917e6bf6ba5c79cfb61
Marco Rossi <marco.rossi@microsoft.com>
2020-01-22 17:25:41 -0800

    Inline var definitions

--- cs/examples/simulator/Program.cs
+++ cs/examples/simulator/Program.cs
@@ -6,21 +6,17 @@ namespace simulator
     { 
         static void Main(string[] args)
         {
-            int numActions, numContexts, tot_iter, mod_iter, rnd_seed, swap_preferences_iter;
-            float minP, maxP, noClickCost, clickCost;
-            string SaveModelPath = "";
-
             string ml_args = args[0];
-            if (!int.TryParse(args[1], out numActions) ||
-                !int.TryParse(args[2], out numContexts) ||
-                !float.TryParse(args[3], out minP) ||
-                !float.TryParse(args[4], out maxP) ||
-                !float.TryParse(args[5], out noClickCost) ||
-                !float.TryParse(args[6], out clickCost) ||
-                !int.TryParse(args[7], out tot_iter) ||
-                !int.TryParse(args[8], out mod_iter) ||
-                !int.TryParse(args[9], out rnd_seed) ||
-                !int.TryParse(args[10], out swap_preferences_iter))
+            if (!int.TryParse(args[1], out int numActions) ||
+                !int.TryParse(args[2], out int numContexts) ||
+                !float.TryParse(args[3], out float minP) ||
+                !float.TryParse(args[4], out float maxP) ||
+                !float.TryParse(args[5], out float noClickCost) ||
+                !float.TryParse(args[6], out float clickCost) ||
+                !int.TryParse(args[7], out int tot_iter) ||
+                !int.TryParse(args[8], out int mod_iter) ||
+                !int.TryParse(args[9], out int rnd_seed) ||
+                !int.TryParse(args[10], out int swap_preferences_iter))
             {
                 Console.WriteLine("Failed to parse input arguments!");
                 Console.WriteLine("Usage: simulator.exe ml_args num_actions num_contexts minP maxP noClickCost clickCost tot_iter mod_iter rnd_seed swap_preferences_iter ml_args2 SaveModelPath");
@@ -29,6 +25,7 @@ namespace simulator
 
             string ml_args2 = args[11];
 
+            string SaveModelPath = "";
             if (args.Length >= 13)
                 SaveModelPath = args[12];
 
